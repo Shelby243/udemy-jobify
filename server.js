8033e64d19f67b9adfcf3b9422806c2114b669ee
Le_Defender <padreshelby@gmail.com>
2023-07-10 03:15:20 +0300

    server set up

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("server running ...");
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is Listening on port ${port}`);
+});
