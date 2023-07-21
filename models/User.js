5411be418ebfd85e3d9b3d17bdfcfc3bfda42702
Le_Defender <padreshelby@gmail.com>
2023-07-21 10:01:41 +0300

    job controllers and models

---
+++ models/User.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import validator from "validator";
+const UserSChema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email ",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+  lastname: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "lastname",
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "my city",
+  },
+});
+export default mongoose.model("User", UserSChema);
